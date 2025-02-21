f4870a3cf69b57785019435bcd6263cdbed12c56
SouravGaur <167708256+SouravGaur@users.noreply.github.com>
2025-02-21 12:43:09 +0530

    hello

---
+++ src/components/experienceSection/ExperienceTopMiddle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ExperienceTopMiddle = () => {
+  return (
+    <div className="lg:w-[45%] md:w-[60%] sm:w-[86%]">
+      <img
+        src="../../public/images/experience-image.png"
+        alt="experience image"
+      />
+    </div>
+  );
+};
+
+export default ExperienceTopMiddle;
