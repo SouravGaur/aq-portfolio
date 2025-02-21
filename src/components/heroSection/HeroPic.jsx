4098da0a0612db9c208b9bcc53a480c40e5eda58
SouravGaur <167708256+SouravGaur@users.noreply.github.com>
2025-02-21 13:09:16 +0530

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -13,7 +13,7 @@ const HeroPic = () => {
       className="h-full flex items-center justify-center"
     >
       <img
-        src="../../public/images/IMG_4940.PNG"
+        src="/IMG_4940.PNG"
         alt="image"
         className="max-h-[450px] w-auto"
       />
